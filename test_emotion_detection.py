1c7b587145e979efe581e02c67c4d2d800b3a43b
Sushil Kumar <20sushilpandey@gmail.com>
2026-06-15 13:42:14 +0530

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,1 +1,44 @@
+import requests
 
+def test_emotion_detection(text_analyze):
+
+    url = ("https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict")
+
+    payload = { "raw_document": {"text": text_analyze}}
+
+    headers = {"grpc-metadata-mm-model-id":"emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(
+        url,
+        json=payload,
+        headers=headers,
+        timeout=10
+    )
+
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+
+    result = response.json()
+
+    emotions = result["emotionPredictions"][0]["emotion"]
+
+    dominant_emotion = max(
+        emotions,
+        key=emotions.get
+    )
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion
+    }
